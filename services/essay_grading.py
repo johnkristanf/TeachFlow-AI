9cab0a6d06f97580f68983a5609330d716e0bbca
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-12 17:19:45 +0800

    feat: implemented fail grading service

--- services/essay_grading.py
+++ services/essay_grading.py
@@ -7,9 +7,12 @@ class EssayGradingService:
         self.repo = EssayGradingRepository()
         
     def grade_essay(self, essay_id, essay: str, rubric_criteria: str):
-        grading_result = self.client.grade_essay(essay, rubric_criteria)
-        parsed_grading_result = self.client.parse_llm_response_to_json(grading_result)
+        raw_grading_result = self.client.grade_essay(essay, rubric_criteria)
+        print(f'raw_grading_result: {raw_grading_result}')
+        
+        parsed_grading_result = self.client.parse_llm_response_to_json(raw_grading_result)
         print(f'parsed_grading_result: {parsed_grading_result}')
+        
         
         evaluations = parsed_grading_result.get('evaluations')
         total_score = parsed_grading_result.get('total_score')
@@ -24,5 +27,8 @@ class EssayGradingService:
         self.repo.save_evaluations(essay_id, evaluations)
         self.repo.save_summary(essay_id, total_score, max_total_score, overall_feedback)
         self.repo.update_essay_raw_sql(essay_id, 'status', 'graded')
+        
+    def set_failed_grading(self, essay_id):
+        self.repo.update_essay_raw_sql(essay_id, 'status', 'failed')
         
        
