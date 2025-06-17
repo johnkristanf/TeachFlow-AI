5940630c55ef82ea5b62cbe2a10e11d613dc78cb
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-18 06:34:54 +0800

    logging in lifespan

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import asyncio
-import traceback
 import uvicorn
 from contextlib import asynccontextmanager
 from consumer import start_rabbitmq_consumer 
 from fastapi import FastAPI
-
 
         
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("🐇 Initializing RabbitMQ Consumer...")
     try:
-        await start_rabbitmq_consumer()  # Wait until it's connected and consuming
+        await start_rabbitmq_consumer()
+        print("📡 RabbitMQ consumer started successfully.")
     except Exception as e:
         print(f"❌ Failed to start RabbitMQ consumer: {e}")
     yield
+
     
 app = FastAPI(lifespan=lifespan)
 
-            
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
