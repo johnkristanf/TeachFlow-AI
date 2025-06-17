b20b9cd43737d183e956e83bcda35577da939630
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-18 07:45:46 +0800

    add logging formatter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-
 echo "Running Alembic migrations..."
-# Assuming alembic.ini is in the current working directory /app
-# Or specify its path: alembic -c /app/alembic.ini upgrade head
-alembic upgrade head
+alembic -c alembic.ini --log-config logging.ini upgrade head
 
 echo "Migrations completed. Starting FastAPI application..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --lifespan on --log-level info
+# Add the --log-config flag to the uvicorn command
+exec uvicorn main:app --host 0.0.0.0 --port 8000 --lifespan on --log-config logging.ini
