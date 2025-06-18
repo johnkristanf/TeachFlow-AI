d0035353bcb2e7ff86be350858e0afac34e313f1
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-18 08:04:10 +0800

    removed logging format in alembic start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 echo "Running Alembic migrations..."
-alembic -c alembic.ini --log-config logging.ini upgrade head
+alembic upgrade head
 
 echo "Migrations completed. Starting FastAPI application..."
-# Add the --log-config flag to the uvicorn command
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --lifespan on --log-config logging.ini
