35440bf2a3bd56451b05d5585278f815bb37c29f
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-22 16:05:17 +0800

    add repository task to thread

--- services/essay_grading.py
+++ services/essay_grading.py
@@ -1,17 +1,16 @@
 from core.openai_client import OpenAIClient
 from repositories.essay_grading import EssayGradingRepository
+import asyncio
 
 class EssayGradingService:
     def __init__(self):
         self.client = OpenAIClient()
         self.repo = EssayGradingRepository()
         
-    def grade_essay(self, essay_id, essay: str, rubric_category, grade_level, grade_intensity, rubric_criteria: str):
-        raw_grading_result = self.client.grade_essay(essay, rubric_category, grade_level, grade_intensity, rubric_criteria)
-        print(f'raw_grading_result: {raw_grading_result}')
+    async def grade_essay_service(self, essay_id, essay: str, rubric_category, grade_level, grade_intensity, rubric_criteria: str):
+        raw_grading_result = await self.client.grade_essay(essay, rubric_category, grade_level, grade_intensity, rubric_criteria)
         
         parsed_grading_result = self.client.parse_llm_response_to_json(raw_grading_result)
-        print(f'parsed_grading_result: {parsed_grading_result}')
         
         
         evaluations = parsed_grading_result.get('evaluations')
@@ -24,11 +23,12 @@ class EssayGradingService:
         print(f'- max_total_score: {max_total_score}')
         print(f'- overall_feedback: {overall_feedback}')
         
-        self.repo.save_evaluations(essay_id, evaluations)
-        self.repo.save_summary(essay_id, total_score, max_total_score, overall_feedback)
-        self.repo.update_essay_raw_sql(essay_id, 'status', 'graded')
+        # PROVIDES A NON_BLOCKING OPERATION OVER SYNCHRONOUS FUNCTIONS
+        await asyncio.to_thread(self.repo.save_evaluations, essay_id, evaluations)
+        await asyncio.to_thread(self.repo.save_summary, essay_id, total_score, max_total_score, overall_feedback)
+        await asyncio.to_thread(self.repo.update_essay_raw_sql, essay_id, 'status', 'graded')
         
-    def set_failed_grading(self, essay_id, failure_type: str, error_message: str, error_details: dict = None):
+    def set_failed_grading_service(self, essay_id, failure_type: str, error_message: str, error_details: dict = None):
         self.repo.update_essay_raw_sql(essay_id, 'status', 'failed')
         self.repo.insert_grading_log(
             essay_id=essay_id,
